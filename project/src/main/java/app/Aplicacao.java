6943290f6b8c4e91dc23d8d4baf09c0023e3f78f
Arthur Goncalves de Moraes <gdemoraesarthur@gmail.com>
2024-04-12 17:32:34 -0300

    app finalizado

---
+++ project/src/main/java/app/Aplicacao.java
@@ -0,0 +1,14 @@
+package app;
+
+import static spark.Spark.*;
+import service.AppService;
+
+public class Aplicacao {
+    private static AppService appService = new AppService();
+    public static void main(String[] args) {
+        port(1234);
+        staticFiles.location("/web");
+
+        get("/home", (request, response) -> appService.load(request, response));
+    }
+}
